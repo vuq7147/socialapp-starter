8c696bee4c5ac085f5d5b5570dd2730dd6c1e3de
Jones <seanjone@amazon.com>
2020-01-16 13:28:20 -0500

    started messages

---
+++ src/components/message/message.js
@@ -0,0 +1,33 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import { userIsNotAuthenticated } from "../HOCs";
+
+class Message extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        
+      };
+    }
+}
+
+render(); { }
+    return (
+      <div>
+        <div>
+          Name: {}
+        </div>
+        <div>
+          content: {}
+        </div>
+        <div>
+          date: {}
+        </div>
+      </div>
+    );
+  
+    
+
+
+      export default userIsNotAuthenticated (Home);

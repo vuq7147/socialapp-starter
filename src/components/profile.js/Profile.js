a3cbcd1a2fec620407d8445d38784e37f46d7dcb
bijucher <53838347+bijucher@users.noreply.github.com>
2020-01-16 13:31:40 -0500

    firstcopy

---
+++ src/components/profile.js/Profile.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { userIsNotAuthenticated } from "../HOCs";
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.client = new Profile()
+        this.state = {
+
+
+        }
+
+    }
+    render() {
+        return (
+            <div>
+                <div>username:{}</div>
+                <div>displayName:{}</div>
+                <div>about:{}</div>
+            </div>
+
+        )
+    }
+
+
+
+}
+
+
+
+
+export default userIsNotAuthenticated(Profile);

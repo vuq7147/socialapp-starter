61008a21b446500c1c9cbeb4190236e61f2320fc
Jones <seanjone@amazon.com>
2020-01-16 16:23:03 -0500

    display something

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -1,32 +1,66 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import { userIsNotAuthenticated } from "../../HOCs";
+// import { userIsNotAuthenticated } from "../../HOCs";
+import BlueService from '../../blueService';
 
 class Message extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
+        this.client = new BlueService();
+        this.state = {
+            data: {},
+        };
+    }
 
-    this.state = {
+    getNewMessage() {
+        return this.client.getMessage().then(result => {
+            console.log(result.data)
+              this.setState({
+            data: result.data
+              })
+        })
+    }
 
-    };
-  }
+    componentDidMount() {
+        this.getNewMessage();
+    }
 
+    render() {
+        if (this.state.data.message) {
+            return (
+                <div>
+                    <div>
+                        message: {this.state.data.message.text}
+                    </div>
+                    <div>
+                        username: {this.state.data.message.username}
+                    </div>
+                    <div>
+                        date: {this.state.data.message.createdAt}
+                    </div>
+                </div>
+            )} 
+        else {
+            return (
+                <div>
+                    
+                </div>
+            )}
 
-  render() {
-    return (
-      <div>
-        <div>
-          Name: {}
-        </div>
-        <div>
-          content: {}
-        </div>
-        <div>
-          date: {}
-        </div>
-      </div>
-    );
-  }
+        // return (
+        //     <div>
+        //         <div>
+        //             Name: {}
+        //         </div>
+        //         <div>
+        //             content: {}
+        //         </div>
+        //         <div>
+        //             date: {}
+        //         </div>
+        //     </div>
+        // );
+    }
 
 }
 
